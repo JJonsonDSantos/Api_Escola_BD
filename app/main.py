f19dd7aece03a5766dd1cc41d8b74c38dd19177a
JJonsonDSantos <jonsondsantos@gmail.com>
2026-02-19 23:25:12 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,11 @@ from app.routers import estudantes, matriculas
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="API Escola")
+app = FastAPI()
 
 app.include_router(estudantes.router)
 app.include_router(matriculas.router)
+
+@app.get("/")
+def root():
+    return {"mensagem": "API Escola funcionando 🚀"}
